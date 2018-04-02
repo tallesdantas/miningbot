9f3d3727b075d4083350b2caca8ba90afed5e663
unknown <talles.d.o.p@gmail.com>
2018-04-02 18:13:02 -0300

    2

--- index.js
+++ index.js
@@ -122,6 +122,10 @@ app.get('/rigs/:tagId', function(req, res) {
 });
 
 
-var listener = app.listen(8080, function(){
-    console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
